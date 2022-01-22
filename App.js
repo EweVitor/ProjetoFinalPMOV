e3f1abaff8ef6bf21e669eadaa97549cf08c0d9a
EweVitor <ifalinformatica18@gmail.com>
2022-01-22 19:45:53 -0300

    trabalho Ultima versão
    trabalho concluído, próxima etapa de testes

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React , {useState, useEffect}from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, FlatList, ScrollView } from 'react-native-web';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button, FlatList, Image, ImageBackground, ScrollView , TouchableOpacity} from 'react-native-web';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { getLivros } from './service/livrosService';
-import AsyncStorage from '@react-native-async-storage/async-storage';s
 
 
-export default function App() {
+export default function App (){
   
   return (
     
@@ -32,9 +31,17 @@ function loginScreen(){
 
   return(
     <View style={styles.container}>
-      <Text>Tela de login</Text>
+    <Text style={styles.title1}>FinanceEduca</Text>
+    <TextInput
+        style={styles.input}
+        placeholder="Digite seu email: " 
+      />
+       <TextInput
+        style={styles.input}
+        placeholder="Digite sua senha:"
+      />
 
-      <Button title="Sair" onPress={() =>{
+      <Button style={styles.btn} title="Entrar" onPress={() =>{
         navigation.replace('homeTabNavigator')
      
         }}></Button>
@@ -49,12 +56,34 @@ function homeScreen(){
 
   return(
     <View style={styles.container}>
-      <Text>Tela de home</Text>
-      <Button title="Sair" onPress={() =>{
-        navigation.replace('login')
-     
-        }}>
-      </Button>
+
+      <View style={styles.materiasStyles}>
+
+        <Text style={styles.title1}>FinanceEduca</Text>
+        <Text style={styles.paragrafo}>Aplicativo voltado para o ensino de finanças a jovens entre 15 e 22 anos.
+          O primeiro passo é começar a entender os princípios da educação financeira para aprender o básico de como lidar com dinheiro.
+
+          A educação financeira pode ser o melhor caminho para enriquecer e formar um patrimônio consistente e trazer tranquilidade para você e sua família. Educação Financeira nada mais é do que a arte de dominar o dinheiro para fazê-lo trabalhar por você.
+
+          Para a ENEF (Estratégia Nacional de Educação Financeira), pessoas bem educadas financeiramente melhoram a sua compreensão em relação ao dinheiro e produtos com informação, formação e orientação. Nesse sentido, geram-se os valores e as competências neces sárias para se tornarem mais conscientes das oportunidades e riscos envolvidos. Para assim poderem fazer escolhas bem informadas.”
+
+          Alguns conceitos que você precisa saber:
+          As três fases da Educação Financeira
+          1º Ganhe, 2° economize e 3° invista. Esse poderia ser o resumo de tudo, pois se você tem dificuldades para economizar, acabará gastando todo o seu dinheiro e não conseguirá investir para aumentar a sua renda. Por isso, pague todas as contas, renegocie dívidas e junte dinheiro até conseguir uma reserva de emergência e uma poupança para investir e montar uma boa carteira de investimentos.
+        </Text>
+        <Text style={styles.title3}>  Conheça os investimentos que aplica </Text>
+        <Text style={styles.paragrafo}>
+          Você não precisa ser rico para investir, pois há diversas opções de investimentos para quem não tem uma alta quantia. Tesouro Direto, CDB, LCI e LCA são alguns investimentos que não exigem um alto valor de início. O investimento em Tesouro Direto, por exemplo, pode ser iniciado com menos de R$ 100.
+
+          Mas é importante conhecer onde você está aplicando o seu dinheiro. O investimento é como um apoio financeiro e você se torna um dono do título que você escolheu ou daquele negócio no qual você virou acionista. Por isso é necessário pesquisar e avaliar se você realmente irá apoiar ou concorda com a atuação daquela empresa ou grupo financeiro onde aportou o seu capital.
+
+        </Text>
+      </View>
+
+      <TouchableOpacity style={styles.floatButton} onPress={()=>{ 
+                  navigation.replace('login')}}>
+        <Ionicons name={'arrow-back-outline'} size={25} color={'#FFF'} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -64,11 +93,15 @@ function contatoScreen(){
 
   return(
     <View style={styles.container}>
-      <Text>fale com nosco</Text>
-      <Button title="Informação" onPress={() => {navigation.navigate("info")}}>
-
-
-      </Button>
+      <Text style={{fontSize:21}}>Fale com nosco</Text>
+      <Text style={styles.item}>Ewerton Desenvolvedor: evls2@aluno.ifal.edu.br</Text>
+      <Text style={styles.item}>Jaqueline Desenvolvedor: js@aluno.ifal.edu.br</Text>
+      <Text style={styles.item}>Luiza Keyla Desenvolvedor: lks@aluno.ifal.edu.br</Text>
+      <Text style={styles.item}>Emyle Vitoria Desenvolvedor: ev@aluno.ifal.edu.br</Text>
+      <TouchableOpacity style={styles.floatButton} onPress={()=>{ 
+                  navigation.navigate('info')}}>
+        <Ionicons name={'call-outline'} size={25} color={'#FFF'} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -77,22 +110,30 @@ function infoScreen(){
 
   return(
     <View style={styles.container}>
-      <Text>tela de informação</Text>
-      <View style={styles.container}>
+      <Text style={styles.title2}>Tela de informação nosso trabalho: </Text>
+      <View style={styles.choroStyle}>
+        <Text>Nosso trabalho é honesto, merecemos pelo menos um 6 ae professor pfv!</Text>
+        <Text>Desculpa a bagunça professor, mas durante os estudos estava dando muitos erros o codigo
+          e acabei chuntando o balde e fazando tudo em unica tela, estava dando muitos erros na parte de navegação fazendo
+          com os componentes em outros arquivos.
 
- 
-
-</View>
+        </Text>
+      </View>
     </View>
   );
 }//materias ====================
 function materiasScreen(){
 
   const [data, setData] = useState([]);
-  const Item =({title, releaseYear})=> (
+  const Item =({title, releaseYear, descricao})=> (
       <View style={styles.item}>
-        <Text>{title}</Text>
-        <Text>{releaseYear}</Text>
+
+        <Text style={styles.titleLivro}>{title}</Text>
+
+        <Text style={styles.descr}>{descricao}</Text>     
+
+       <Text style={styles.releaseYear}>Ano de laçamento {releaseYear} </Text> 
+
       </View>
 
   );
@@ -118,14 +159,19 @@ function materiasScreen(){
     <View style={styles.materiasStyles}>
       <Text style={styles.title2}>Livros sobre educação financeira</Text>
       <Text style={styles.title3}>Abaixo temos alguns livros para estudo sobre a educação finaceira</Text>
-              
-      <FlatList style={styles.filmes}
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o nome do livro:"
+      />
+      
+      <FlatList style={styles.telaLivros}
           data={data}
           renderItem={({item}) =>(
-            <Item title={item.title} releaseYear={item.releaseYear}></Item>
-
+            <Item title={item.title} releaseYear={item.releaseYear} descricao={item.descricao}> </Item>
+            
           )} 
         >
+          
       </FlatList>
     </View>
   );
@@ -149,19 +195,22 @@ function homeTabNavigator(){
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
-          if (route.name === 'home') {
+          if (route.name === 'Home') {
             iconName = focused ? 'information-circle' : 'information-circle-outline';
           } else if (route.name === 'Sobre nois') {
             iconName = focused ? 'list-outline' : 'list';
+          }else if(route.name ==='Materias'){
+            iconName = focused ? 'newspaper-outline' : 'newspaper';
+
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: 'tomato',
+        tabBarActiveTintColor: '#6FA95F',
         tabBarInactiveTintColor: 'gray',
     })}>
 
-    <Tab.Screen name="home" component={homeScreen}></Tab.Screen>
+    <Tab.Screen name="Home" component={homeScreen}></Tab.Screen>
     <Tab.Screen name="Materias" component={materiasScreen}></Tab.Screen>
     <Tab.Screen name="Sobre nois" component={ContateStackNavigatorComponente} options={{headerShown :false}}></Tab.Screen>
   </Tab.Navigator>
@@ -175,17 +224,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  paragrafo:{
+    margin:15,
+    backgroundColor: '#CFFFC2',
+    padding:13,
+    maxHeight:300,
 
-  filmes:{
-    marginTop:10,
+
+  },
+  telaLivros:{
+    flex:1,
+    maxHeight:320,
+    marginBottom:30
+    
+  },
+  title1:{
+    fontSize:30,
+    
 
   },
   title2:{
     alignContent:'center',
-    fontSize:25,
+    fontSize:33,
     margin:15,
-    backgroundColor:'red',
+    backgroundColor:'#90CD7F',
     padding:10,
     borderRadius:20.0
 
@@ -197,12 +261,67 @@ const styles = StyleSheet.create({
   },
   materiasStyles:{
     margin:10,
-
+   
   },
   item:{
-    backgroundColor:'red',
-    padding:5,
-    margin :5,
+    backgroundColor:'#C3FFB3',
+    padding:8,
+    margin :5, 
+    marginHorizontal:50,
+    
+    borderTopLeftRadius:20,
+    borderBottomRightRadius:20
+  },
+  titleLivro:{
+    fontSize:23,
+    margin: 5,
+    
+
+  },
+  releaseYear:{
+    margin:7,
+    marginLeft:3,
+    fontSize:15
+
+  },
+  descr:{
+    margin:15,
+
+  },
+
+  input:{
+
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderTopLeftRadius:10,
+    borderBottomRightRadius:10
+  },
+  floatButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    height: 50,
+    backgroundColor: '#87D073',
+    borderRadius: 100,
+
+  },choroStyle:{
+    margin:30,
+    backgroundColor: '#AAC1A4',
+    padding:10, 
+    fontSize:13,
+    width:500
+
+  },btn:{
+    width: 13,
+    backgroundColor:'#AAC1A4',
+    borderRadius: 0,
+
 
   }
+
 });
--- service/livrosService.js
+++ service/livrosService.js
@@ -1,11 +1,14 @@
-
-
 const livros = [
-    { "id": "1", "title": "Aprendendo a mexer com a mente.", "releaseYear": "1977" },
-    { "id": "2", "title": "O dinheiro é so uma questão?", "releaseYear": "1985" },
-    { "id": "3", "title": "Treine sua mente!", "releaseYear": "1999" },
-    { "id": "4", "title": "Seja melhor que ontem.", "releaseYear": "2010" },
-    { "id": "5", "title": "O poder da mente!", "releaseYear": "2014" }
+    { "id": "1", "title": "1 – O Almanaque do Pobre Charlie – a inteligência e sabedoria de Charles T. Munger", "releaseYear": "1977", 
+    "descricao":"A obra reúne os melhores discursos, palestras, comentários e ensinamentos sobre investimentos e a vida de Charlie Munger, um dos maiores nomes dos investimentos de valor de todos os tempos.Há um trecho no livro que apresenta o checklist de Munger para investir. São dez áreas que o braço direito de Warren Buffett leva em consideração em suas tomadas de decisões." },
+    { "id": "2", "title": "2 – Previsivelmente irracional", "releaseYear": "1985",
+    "descricao":"Dan Ariely utiliza experimentos para comprovar o quanto somos suscetíveis à irracionalidade e o quanto isso afeta nossa vida e decisões. Através de exemplos do cotidiano, o autor quebra paradigmas racionais da economia tradicional e mostra que seguir os conceitos da economia comportamental é o caminho para entendermos mais sobre nós mesmos e, consequentemente, nos tornarmos investidores mais conscientes e, por que não, mais racionais." },
+    { "id": "3", "title": "3 – Contrarian Investment Strategies: beat the market by going against the crowd", "releaseYear": "1999",
+    "descricao": "Este livro ainda não tem uma edição brasileira, mas em tradução livre seria algo como Estratégias de Investimento Contrário: supere o mercado indo contra a multidão. Nele, David Dreman reúne 41 regras que devem ser respeitadas se você deseja ter uma postura oposta à da maioria dos que investem em ações. Se quiser saber mais sobre o livro (recomendo), leia este artigo. Aqui estão algumas de minhas regras favoritas: Regra 1 – Respeite a dificuldade de lidar com uma infinidade de informações. Poucos conseguem fazer isso com sucesso. Regra 2 – Não existem segmentos altamente previsíveis em que você pode traçar previsões 100% seguras. Contar com isso pode lhe causar problemas." },
+    { "id": "4", "title": "4 – O poder do hábito", "releaseYear": "2010",
+    "descricao":"Outro livro que pode ajudá-lo a compreender melhor as armadilhas criadas por sua cabeça é O poder do hábito – Por que fazemos o que fazemos na vida e nos negócios, escrito por Charles DuhiggNele, o autor explica a origem dos hábitos – bons e ruins – e mostra, através de exemplos e experimentos práticos, como você pode criar o cenário para transformar seus hábitos."},
+    { "id": "5", "title": "5 – A lógica do mercado: como lucrar com finanças comportamentais", "releaseYear": "2014",
+    "descricao":"John R. Nofsinger, doutor e professor de Finanças da Washington State University, aborda vários conceitos importantes para quem está buscando controlar emoções ao investir e ser mais racional em seus investimentos neste livro. Já escrevi um artigo sobre ele, inclusive. Abaixo, resumo algumas de minhas lições favoritas:1 – Saiba por que está investindo.2 – Antes de comprar uma ação, compare os critérios da empresa em que está investindo com os seus próprios.3 – Diversifique seus investimentos.4 – Crie hábitos e evite tentações." }
   ]
 
   function getLivros() {
